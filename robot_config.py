4911be99551a0c3bcc57ccbf5882f86311085aba
leahschneidereit <118493180+leahschneidereit@users.noreply.github.com>
2023-02-20 21:49:05 -0500

    Add files via upload

--- robot_config.py
+++ robot_config.py
@@ -1,32 +1,112 @@
-# import necessary modules 
+# impot robot model module
 import robot_model
+
+#import math modules
 import math
 import numpy as np
 
-# set print formatting
-np.set_printoptions(precision = 2, suppress = True)
+#set print formatting
+np.set_printoptions(precision = 1, suppress = True)
 
-# test 1
-# set parameters for 1st link
-alpha1= 1
+
+#--------------TEST 1---------------#
+print("Test 1")
+print()
+
+# set dh parameters for 1st link
+a1 = 1
+alpha1= 0
 theta1 = math.pi/2
-a1 = 0
-d1 = 0.1625
+d1 = 0
 
 #set parameters for second link
-alpha2 = 1
-theta2 =math.pi/2
-a2 = -0.425
+a2 = 1
+alpha2 = 0
+theta2 = math.pi/2
 d2 = 0
 
-# call kinematic chain function with 2D array of the dh parameters 
-H = robot_model.kinematic_chain([[a1,alpha1,d1,theta1],[a2, alpha2,d2,theta2]])
+# call kinematic_chain to calculate homogeneous transformation matrix, H 
+H = robot_model.kinematic_chain([[a1,alpha1,theta1,d1],[a2, alpha2,theta2,d2]])
 
-# call get_pos and pass H from kinematic chain function to determine x,y,and z locations
+# call get_pos and pass Hto determine x,y,and z values 
 pos = robot_model.get_pos(H)
-print(pos)
+print("x,y,z: ", pos)
 
-# call get_rot and pass H from kinematic function to determine roll-pitch-yaw
-# angle values 
+# call get_rot and pass H to determine roll-pitch-yaw angle values 
 rot = robot_model.get_rot(H)
-print(rot)
+print("roll, pitch, yaw: ",rot)
+
+#formatting
+print()
+print()
+
+#----------------TEST 2-----------------#
+print("Test 2")
+print()
+
+#set dh parameters for each link (1-6)
+a1 = 0
+alpha1= math.pi/2
+theta1 = 0
+d1 = .1625
+
+a2 = -0.425
+alpha2= 0
+theta2 = 0
+d2 = 0
+
+a3 = -0.3922
+alpha3= 0
+theta3 = 0
+d3 = 0
+
+a4 = 0
+alpha4= math.pi/2
+theta4 = 0
+d4 = 0.1333
+
+a5 = 0
+alpha5= -math.pi/2
+theta5 = 0
+d5 = 0.0997
+
+a6 = 0
+alpha6= 0
+theta6 = 0
+d6 = 0.0966
+
+#call kinematic chain to determine final transformation matrix 
+H = robot_model.kinematic_chain([[a1,alpha1,theta1,d1],[a2, alpha2,theta2,d2],[a3,alpha3,theta3,d3],
+                                 [a4,alpha4,theta4,d4],[a5,alpha5,theta5,d5],[a6,alpha6,theta6,d6]])
+
+# call get_pos and pass H to determine x,y,z values from transformation matrix
+pos = robot_model.get_pos(H)
+print("x,y,z: ", pos)
+
+# call get_rot and pass H to determine roll, pitch, and yaw angle values
+rot = robot_model.get_rot(H)
+print("roll, pitch, yaw: ",rot)
+
+#formatting
+print()
+print()
+
+#-----------------Test 3--------------------#
+print("Test 3")
+print()
+
+# theta2 is only value that changes from case1 - set new value for theta2
+theta2 = -math.pi/2
+
+
+#call kinematic chain to determine transformation matrix 
+H = robot_model.kinematic_chain([[a1,alpha1,theta1,d1],[a2, alpha2,theta2,d2],[a3,alpha3,theta3,d3],
+                                 [a4,alpha4,theta4,d4],[a5,alpha5,theta5,d5],[a6,alpha6,theta6,d6]])
+
+# call get_pos and pass H to determine x,y,z position
+pos = robot_model.get_pos(H)
+print("x,y,z: ", pos)
+
+# call get_rot and pass H to calculate roll-pitch-yaw angle values 
+rot = robot_model.get_rot(H)
+print("roll, pitch, yaw: ",rot)
--- robot_model.py
+++ robot_model.py
@@ -20,17 +20,19 @@ def kinematic_chain (dh_params):
     parameters: list of lists of dh_params in this order: a, alpha, d, theta
     returns: final array for the kinematic chain
     '''
+    # initialize 4x4 identity matrix 
     H = np.identity(4)
+
+    # iterate over list of params for each link, set value to corresponding dh parameter, then multiply
+    # to calculate final matrix 
     for row in dh_params:
         a = row[0]
         alpha = row[1]
-        d = row[2]
-        theta = row[3]
-        H_i = np.array([[math.cos(theta),-math.sin(theta)*math.cos(alpha),
-                     math.sin(theta)*math.sin(alpha),a * math.cos(theta)],
-                    [math.sin(theta),math.cos(theta)*math.cos(alpha),-math.cos(theta)*math.sin(alpha),
-                     a * math.sin(theta)],[0, math.sin(alpha), math.cos(alpha),d],[0,0,0,1]])
+        theta = row[2]
+        d = row[3]
+        H_i = dh_transformation(a,alpha,theta,d)
         H = np.matmul(H, H_i)
+
     return H
 
 def get_pos(trans_matrix):
@@ -39,9 +41,11 @@ def get_pos(trans_matrix):
     parameters: transformation matrix
     returns: (x,y,z)
     '''
-    h14 = trans_matrix.item(0,3)
-    h24 = trans_matrix.item(1,3)
-    h34 = trans_matrix.item(2,3)
+    #extract values from matrix
+    h14 = round(trans_matrix.item(0,3),2)
+    h24 = round(trans_matrix.item(1,3),2)
+    h34 = round(trans_matrix.item(2,3),2)
+    
     return h14, h24, h34
 
 def get_rot(trans_matrix):
@@ -50,15 +54,17 @@ def get_rot(trans_matrix):
     parameters: transformation matrix
     returns: psi, theta, and phi values
     '''
+    # extract values from matrix 
     h11 = trans_matrix.item(0,0)
     h21 = trans_matrix.item(1,0)
     h31 = trans_matrix.item(2,0)
     h32 = trans_matrix.item(2,1)
     h33 = trans_matrix.item(2,2)
 
-    psi = np.arctan(h32/h33)
-    theta = np.arctan(-h31 / (math.sqrt(h32**2+h33**2)))
-    phi = np.arctan(h21/h11)
+    # mathematical equations to calculate angle values 
+    psi = round(math.atan2(h32,h33),2)
+    theta = round(math.atan2(-h31, math.sqrt(h32**2+h33**2)),2)
+    phi = round(math.atan2(h21,h11),2)
 
     return psi, theta, phi
     
